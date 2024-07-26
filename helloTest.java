fe1be71e42de64be96b8f0dd3ba30d8356507f24
Journey-2 <167363028+Journey-2@users.noreply.github.com>
2024-07-26 11:03:56 +0530

    Update helloTest.java

--- helloTest.java
+++ helloTest.java
@@ -2,6 +2,6 @@ class helloTest
 {
     public static void main(String []args)
     {
-        System.out.println("My Fifth Java Program.");
+        System.out.println("My sixth Java Program.");
     }
 };
